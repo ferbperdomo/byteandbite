8f6f2276f0dd7a59e85b605506c714748e1a70d0
Cristian Perdomo <ferperdomo14@gmail.com>
2025-10-01 20:34:08 +0200

    fix: disable image optimization for local media

--- next.config.ts
+++ next.config.ts
@@ -18,8 +18,7 @@ const nextConfig: import("next").NextConfig = {
     minimumCacheTTL: 31536000, // 1 year cache
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    unoptimized: false,
-    loader: "default",
+    unoptimized: true,
   },
   compress: true,
   poweredByHeader: false,
